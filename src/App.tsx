b9d78d3742d1f0f1e14c5d8f6b623837c1b564f8
Hanson <chansonh@defencesg.mobi>
2024-04-04 12:38:24 +0800

    Create todo submit form and display tasks UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
+import styled from 'styled-components';
 import Form from "./components/Form/Form"
+import ToDoList from "./components/ToDoList/ToDoList"
+
+const Theme = styled.div`
+  background: linear-gradient(#64d8d1, #4e8fe3);
+  min-height: 95vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: 'Helvetica', sans-serif;
+  color: #000000;
+  letter-spacing: 0.2px;
+`;
+
+const FormBackground = styled.div`
+  background-color: #FFFFFF;
+  width: 500px;
+  height: 70vh;
+  padding: 30px;
+  margin: 0;
+  border-radius: 8px;
+`;
 
 function App() {
-
+  
   return (
-    <>
-      <h2>App page</h2>
-      <Form/>
-    </>
+    <Theme>
+      <FormBackground>
+        <Form/>
+        <ToDoList/>
+      </FormBackground>
+    </Theme>
   )
 }
 
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,26 +1,48 @@
+import styled from 'styled-components';
+
+const FormContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 30px;
+`
+
+const TextBox = styled.input `
+    color: #ffffff; 
+    font-size: 18px;
+    width: 400px;
+    height: 23px;
+    padding: 12px;
+    border-radius: 3px;
+    border: 1px solid #D0D0D0;
+`
+
+const ButtonSubmit = styled.button `
+    background-color: #8d4be4;
+    color: #ffffff;
+    width: 50px;
+    height: 50px;
+    font-size: 40px;
+    border-radius: 3px;
+    border: none;
+    margin-left: 10px;
+`
+
 function Form() {
-
-
     return (
-        <>
-        <h2>Form Page</h2>
-        <form>
-        <label>
-            Name: 
-            <input type="text" name="name" />
-        </label>
-        <p></p>
-        <label>
-            Task: 
-            <input type="text" name="name" />
-        </label>
-        <p></p>
-        <button type="submit">Submit</button>
-
-
-        </form>
-
-        </>
+        <div>
+            <h1>Todo App</h1>
+                <form>
+                    <FormContainer>
+                        <TextBox 
+                            type="text"
+                            placeholder="Add your new todo"
+                            name="name"
+                        />
+                        <ButtonSubmit type="submit">+</ButtonSubmit>
+                    </FormContainer>
+                </form>
+     
+        </div>
     )
 }
 
