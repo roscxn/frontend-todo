66116d5eb5f2d56a342add86a1febb9c7958ea43
Hanson <chansonh@defencesg.mobi>
2024-04-04 08:33:22 +0800

    Setup Vite React TS, Storybook, Components folders

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Form from "./components/Form/Form"
+
+function App() {
+
+  return (
+    <>
+      <h2>App page</h2>
+      <Form/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,27 @@
+function Form() {
+
+
+    return (
+        <>
+        <h2>Form Page</h2>
+        <form>
+        <label>
+            Name: 
+            <input type="text" name="name" />
+        </label>
+        <p></p>
+        <label>
+            Task: 
+            <input type="text" name="name" />
+        </label>
+        <p></p>
+        <button type="submit">Submit</button>
+
+
+        </form>
+
+        </>
+    )
+}
+
+export default Form;
